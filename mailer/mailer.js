82785cb4aa03b112e9923467a7c2921c2d65ca7b
Christian Downs <christian.downs.15@gmail.com>
2024-02-29 15:34:46 -0600

    Added full email support

---
+++ mailer/mailer.js
@@ -0,0 +1,30 @@
+function sendMail(email, subject, message, res) {
+  var nodemailer = require("nodemailer");
+
+  var nodemailer = require("nodemailer");
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_AUTH_USER,
+      pass: process.env.MAIL_AUTH_PASSWORD,
+    },
+  });
+  var mailOptions = {
+    from: process.env.MAIL_AUTH_USER,
+    to: email + ", " + process.env.MAIL_AUTH_USER,
+    subject: subject,
+    text: message,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.status(401).json({ success: false });
+      console.log(error);
+    } else {
+      res.status(200).json({ success: true });
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
+
+module.exports = sendMail;
