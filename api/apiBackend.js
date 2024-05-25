0bfa88dad1e781acac7fb993c9c4cfa891d6e250
Christian Downs <christian.downs.15@gmail.com>
2024-05-25 15:02:35 -0500

    added cors policy

--- api/apiBackend.js
+++ api/apiBackend.js
@@ -6,10 +6,22 @@ const port = 3001;
 
 const sendMail = require("../mailer/mailer.js");
 const { addToSweepstakes, getAllSweepstakes } = require("../sweepstakes/sweepstakes.js");
-app.use(express.json());
-app.use(cors());
 
-app.post("/api/sendEmail", (req, res) => {
+
+const corsOptions = {
+  origin: [
+    "https://memorablenights.vip",
+    "https://inventory-management-3kwm.vercel.app/",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+app.post("/api/sendEmail.js", (req, res) => {
   console.log(process.env.MAIL_AUTH_USER, process.env.MAIL_AUTH_PASSWORD);
   console.log(req.body);
   const { email, subject, message } = req.body;
@@ -18,7 +30,7 @@ app.post("/api/sendEmail", (req, res) => {
   
 });
 
-app.post("/api/addToSweepstakes", async (req, res) => {
+app.post("/api/addToSweepstakes.js", async (req, res) => {
   try {
     const { email, name, phone_number, address } = req.body;
     const person = { email:email, name:name, phone_number:phone_number, address:address };
